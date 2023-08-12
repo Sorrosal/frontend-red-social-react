38f689d51c3b18cc1f8e4bc8d878877763869f2e
Sergio Sorrosal <sergiosorrosalgayan@gmail.com>
2023-08-12 09:38:59 +0200

    Fixes

--- src/components/user/People.jsx
+++ src/components/user/People.jsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import avatar from '../../assets/img/user.png';
 import { Global } from '../../helpers/Global';
+import UserList from './UserList';
+
 export default function People() {
+
     const [users, setUsers] = useState([]);
     const [page, setPage] = useState(1);
     const [more, setMore] = useState(true);
+    const [following, setFollowing] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         getUsers(1);
     }, []);
+
     const getUsers = async (nextPage = 1) => {
+        debugger;
         // Efecto de carga
         setLoading(true);
         // Petición para sacar usuarios
@@ -30,19 +35,17 @@ export default function People() {
                 newUsers = [...users, ...data.users];
             }
             setUsers(newUsers);
+            setFollowing(data.user_following);
             setLoading(false);
         }
         // Paginacion
-        if (users.length >= data.total) {
+        if (users.length >= (data.total - data.users.length)) {
             setMore(false);
         }
     }
 
-    const nextPage = () => {
-        let next = page + 1;
-        setPage(next);
-        getUsers(next);
-    }
+
+
     return (
         <>
 
@@ -51,58 +54,18 @@ export default function People() {
 
             </header>
 
-            <div className="content__posts">
-
-                {users.map(user => {
-                    return (
-                        <article className="posts__post" key={user._id}>
-
-                            <div className="post__container">
-
-                                <article className="post__image-user">
-                                    <a href="#" className="post__image-link">
-                                        {user.image != "default.png" && <img src={Global.url + "user/avatar/" + user.image} className="post__user-image" alt="Foto de perfil" />}
-                                        {user.image == "default.png" && <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
-                                    </a>
-                                </article>
-
-                                <div className="post__body">
-
-                                    <div className="post__user-info">
-                                        <a href="#" className="user-info__name">{user.name} {user.surname}</a>
-                                        <span className="user-info__divider"> | </span>
-                                        <a href="#" className="user-info__create-date">{user.created_at}</a>
-                                    </div>
-
-                                    <h4 className="post__content">{user.bio}</h4>
-
-                                </div>
-
-                            </div>
+            <UserList
+                users={users}
+                getUsers={getUsers}
+                following={following}
+                setFollowing={setFollowing}
+                page={page}
+                setPage={setPage}
+                more={more}
+                loading={loading}
+            />
 
 
-                            <div className="post__buttons">
-
-                                <a href="#" className="post__button post__button--green">
-                                    Seguir
-                                </a>
-                                {/* <a href="#" className="post__button post__button--green">
-                            Dejar de eguir
-                        </a> */}
-
-                            </div>
-
-                        </article>
-                    );
-                })}
-
-            </div>
-            {loading ? <div>Cargando...</div> : ""}
-            {more && <div className="content__container-btn">
-                <button className="content__btn-more-post" onClick={nextPage}>
-                    Ver mas personas
-                </button>
-            </div>}
 
             <br />
         </>
